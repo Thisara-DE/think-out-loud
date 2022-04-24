30c0c3e747363e7555207eb0921935e110246d88
Glorzo <thisaraeranga@gmail.com>
2022-04-24 10:46:21 -0500

    finished thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,36 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            validate: {
+                maxlength: 280
+            }
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
+
 const ThoughtSchema = new Schema(
     {
         thoughtText: {
@@ -20,7 +50,7 @@ const ThoughtSchema = new Schema(
             type: String,
             required: true
         },
-        reactions: [reactionsSchema]
+        reactions: [ReactionSchema]
     },
     {
         toJSON: {
